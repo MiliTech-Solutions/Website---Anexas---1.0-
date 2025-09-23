a911d72399377c2a54a9432815a0e2544e0cc57e
MiliTech <militechsoln@gmail.com>
2025-09-23 08:16:27 +0000

    adjustment to significantly reduce that top negative space in the hero s

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
         <div 
             className="absolute inset-0 -z-10 h-full w-full bg-background bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(30,144,255,0.1),rgba(255,255,255,0))]">
         </div>
-      <div className="container mx-auto px-4 md:px-6 h-full flex-grow flex flex-col pt-12">
+      <div className="container mx-auto px-4 md:px-6 h-full flex-grow flex flex-col">
         <div className="grid md:grid-cols-12 gap-8 items-center h-full">
           
           <div className="md:col-span-2">
