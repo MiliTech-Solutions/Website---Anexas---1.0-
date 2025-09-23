2b1374c144f7b49ca2f6e22ab78d7d813d4f3d7e
MiliTech <militechsoln@gmail.com>
2025-09-23 09:37:52 +0000

    add this section above our analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import FinestWork from '@/components/landing/finest-work';
 import Testimonials from '@/components/landing/testimonials';
 import GetAQuote from '@/components/landing/get-a-quote';
 import ByTheNumbers from '@/components/landing/by-the-numbers';
+import InnovationForte from '@/components/landing/innovation-forte';
 
 export default function Home() {
   return (
@@ -27,6 +28,7 @@ export default function Home() {
         <BrandingPackages />
         <FinestWork />
         <Testimonials />
+        <InnovationForte />
         <ByTheNumbers />
         <GetAQuote />
         {/* The sections below are currently not visible in the new design.
