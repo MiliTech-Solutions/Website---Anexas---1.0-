601bf26b804ab8efdf128dfb04928f7d19691be0
MiliTech <militechsoln@gmail.com>
2025-09-23 08:33:14 +0000

    now for the website background implement a gradient effect from the webs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import GetAQuote from '@/components/landing/get-a-quote';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
         <Hero />
