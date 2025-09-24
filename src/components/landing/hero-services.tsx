46d2e322ab379fdf9021c9b99770b5ecf7b24835
MiliTech <militechsoln@gmail.com>
2025-09-24 07:13:11 +0000

    make this section better mobile responsive (_for element <HeroServices>_

--- src/components/landing/hero-services.tsx
+++ src/components/landing/hero-services.tsx
@@ -12,7 +12,7 @@ const services = [
 
 export default function HeroServices() {
   return (
-    <div className="flex flex-col gap-4 text-left">
+    <div className="hidden md:flex flex-col gap-4 text-left">
         <p className="text-[10px] font-bold tracking-widest bg-gradient-to-r from-cyan-400 to-blue-500 text-transparent bg-clip-text mb-2">OUR SERVICES</p>
         {services.map((service, index) => (
             <div key={index} className="flex items-center gap-3">
