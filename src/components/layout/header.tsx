1173aca0e957c149cb41b7063851f659ce57253b
MiliTech <militechsoln@gmail.com>
2025-09-24 17:42:02 +0000

    Make about to about us

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -17,7 +17,7 @@ const navLinks = [
   { href: '/', label: 'Home' },
   { href: '#services', label: 'Services' },
   { href: '#portfolio', label: 'Portfolio' },
-  { href: '#about', label: 'About' },
+  { href: '#about', label: 'About Us' },
 ];
 
 const Logo = () => (
