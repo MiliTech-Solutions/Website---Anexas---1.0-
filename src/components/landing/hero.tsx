574d51e3665e0ecb064751db23adaa564fd6f1b0
MiliTech <militechsoln@gmail.com>
2025-09-24 17:20:13 +0000

    Not implemented

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
                 <TypeAnimation
                   sequence={[
                     'We create stunning digital experiences that drive growth and elevate your brand in the digital landscape.',
-                    1000
+                    1000,
                   ]}
                   wrapper="span"
                   speed={50}
