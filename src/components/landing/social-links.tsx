e182d6cac9a07a095a10237e8e2a8d4484e4febb
MiliTech <militechsoln@gmail.com>
2025-09-24 13:33:24 +0000

    the icfons in the hero section should have the same effect as the get st

--- src/components/landing/social-links.tsx
+++ src/components/landing/social-links.tsx
@@ -2,6 +2,7 @@
 
 import { Facebook, Twitter, Linkedin, Instagram } from 'lucide-react';
 import { Slide } from "react-awesome-reveal";
+import { MouseEvent } from 'react';
 
 const socialLinks = [
     { icon: Facebook, href: '#' },
@@ -12,11 +13,18 @@ const socialLinks = [
 
 
 export default function SocialLinks() {
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (navigator.vibrate) {
+      // Vibrate for 50ms
+      navigator.vibrate(50);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center gap-16">
         <Slide direction="right" triggerOnce cascade damping={0.1} delay={300}>
           {socialLinks.map((social, index) => (
-              <a key={index} href={social.href} className="text-muted-foreground hover:text-cyan-400 transition-all duration-300 hover:scale-150 active:scale-175">
+              <a key={index} href={social.href} className="text-muted-foreground hover:text-cyan-400 transition-all duration-300 hover:scale-150 active:scale-90" onClick={handleClick}>
                   <social.icon className="w-4 h-4" />
               </a>
           ))}
